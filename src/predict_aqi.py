41f4b54cefb02b8efc6e20c8933d1681e34f0d37
Gabriel Julio <gabijajulio179@gmail.com>
2025-07-07 10:49:48 +0100

    Feat: Predict new data

---
+++ src/predict_aqi.py
@@ -0,0 +1,54 @@
+import argparse
+import pandas as pd
+from tensorflow.keras.models import load_model
+
+# Columns used during training
+X_COLS = [
+    "NO2 Mean",
+    "O3 Mean",
+    "SO2 Mean",
+    "CO Mean",
+    "SO2_Mean_Imputed",
+    "CO_Mean_Imputed",
+    "NO2_to_SO2",
+    "CO_to_SO2",
+    "O3_to_CO",
+    "NO2 Mean_roll_3",
+    "O3 Mean_roll_3",
+    "SO2 Mean_roll_3",
+    "CO Mean_roll_3",
+    "year",
+    "month",
+    "is_weekend",
+]
+
+
+def predict(input_csv: str, model_path: str = "../models/best_aqi:model.keras") -> None:
+    # Load input CSV
+    df = pd.read_csv(input_csv)
+
+    # Extract features
+    X = df[X_COLS].values
+
+    # Load trained model (Includes build-in Normalization Layer)
+    model = load_model(model_path)
+
+    # Predicts
+    preds = model.predict(X)
+
+    # Format results
+    pred_df = pd.DataFrame(preds, columns=["AQI_NO2", "AQI_03", "AQI_SO2", "AQI_CO"])
+    outut_df = pd.concat([df.reset_index(drop=True), pred_df], axis=1)
+
+    # Save
+    output_path = input_csv.replace(".csv", "_predicted.csv")
+    outut_df.to_csv(output_path, index=False)
+    print(f"Predictions saved to: {output_path}")
+    print(outut_df[["AQI_NO2", "AQI_03", "AQI_SO2", "AQI_CO"]].head())
+
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description="Run AQI predictions on new data")
+        parser.add_argument("--input", required=True, help="Path to input CSV file")
+        args = parser.parse_args()
+
+        predict(args)
