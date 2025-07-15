464e681598d1cb90c818f57555a94966fc93da2a
Gabriel Julio <gabijajulio179@gmail.com>
2025-07-15 10:53:45 +0100

    Refactor data processing to include reindexing of DataFrame columns for AQI predictions

--- src/api/main.py
+++ src/api/main.py
@@ -3,6 +3,25 @@ from src.api.schema import AQIRequest
 from src.api.predictor import predict_aqi_from_df
 import pandas as pd
 
+X_cols = [
+    "NO2 Mean",
+    "O3 Mean",
+    "SO2 Mean",
+    "CO Mean",
+    "SO2_Mean_Imputed",
+    "CO_Mean_Imputed",
+    "NO2_to_SO2",
+    "CO_to_SO2",
+    "O3_to_CO",
+    "NO2 Mean_roll_3",
+    "O3 Mean_roll_3",
+    "SO2 Mean_roll_3",
+    "CO Mean_roll_3",
+    "year",
+    "month",
+    "is_weekend",
+]
+
 app = FastAPI()
 
 
@@ -22,6 +41,8 @@ def root():
 def predict(request: AQIRequest):
 
     df = pd.DataFrame([item.model_dump(by_alias=True) for item in request.data])
+
+    df = df.reindex(columns=X_cols)
 
     preds = predict_aqi_from_df(df)
 
